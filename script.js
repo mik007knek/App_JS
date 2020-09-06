7d1b4048dc84b094b0ad047b80f3801dc386c4c4
Mishanya <pupik.vasya13@gmail.com>
2020-09-06 09:15:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var numberOfFilms = prompt("Сколько фильмов вы уже посмотррели?", "");
+
+
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: null,
+    actors: null,
+    genres: [],
+    privat: false
+};
+
+var lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+var mark = prompt("На сколько вы его оцените?", "");
+
+personalMovieDB.movies[lastFilm] = mark;
