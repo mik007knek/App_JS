f494ae275ca8a60af4d27058463b5dbbc9edc6b6
Knek <example@gmail.com>
2020-09-06 10:28:39 +0300

    second task succesful

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var numberOfFilms = +prompt("Сколько фильмов вы уже посмотррели?", "");
+var numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 
 var personalMovieDB = {
@@ -11,14 +11,28 @@ var personalMovieDB = {
     privat: false
 };
 
-let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-let mark = prompt("На сколько вы его оцените?", "");
+for (let i = 0; i < 3; i++){
+    let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+        mark = prompt("На сколько вы его оцените?", ""),
+        check = lastFilm.length < 50;
+    while (!(check)||!(lastFilm)||!(mark)){
+        lastFilm = 
+        prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз\nОдин из последних просмотренных фильмов?", "");
+        mark = prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз\nНа сколько вы его оцените?", "");
+        check = (lastFilm.length < 50);
+    }
+    personalMovieDB.movies[lastFilm] = mark;
+}
 
-personalMovieDB.movies[lastFilm] = mark;
+if (personalMovieDB.count < 10){
+    alert("Вы смотрите мало фильмов");
+} else if ((10 < personalMovieDB.count) && (personalMovieDB.count < 30)){
+    alert("Вы классический зритель");
+} else if (30 < personalMovieDB.count) {
+    alert("Вы киноман!");
+} else {
+    alert("Ошибка");
+}
+console.log(personalMovieDB.count);
 
-lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-mark = prompt("На сколько вы его оцените?", "");
-
-personalMovieDB.movies[lastFilm] = mark;
-
-console.log(personalMovieDB);
+//console.log(personalMovieDB);
