e7c593a0a35d1b43d1fcf6b14cd441c2edf9fbe7
Knek <example@gmail.com>
2020-10-06 08:59:11 +0300

    sucessful 4 exercise

--- script.js
+++ script.js
@@ -1,70 +1,78 @@
 "use strict";
 
-let numberOfFilms;
-
-function start(){
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    while (numberOfFilms == '' || numberOfFilms == null || typeof(numberOfFilms) != typeof(123)){
-        numberOfFilms = 
-        +prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз, пожалуйста!\nСколько фильмов вы уже посмотрели?", "");
-    }
-    personalMovieDB.count = numberOfFilms;
-}
-
-function rememberFilms(){
-    for (let i = 0; i < 2; i++){
-        let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-            mark = prompt("На сколько вы его оцените?", ""),
-            check = lastFilm.length < 50;
-        while (!(check)||!(lastFilm)||!(mark)){
-            lastFilm = 
-            prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз, пожалуйста!\n" + 
-            "Один из последних просмотренных фильмов?", "");
-            mark = prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз, пожалуйста!" + 
-            "\nНа сколько вы его оцените?", "");
-            check = (lastFilm.length < 50);
-        }
-        personalMovieDB.movies[lastFilm] = mark;
-    }
-}
-
-function isKinoman(count){
-    if (count < 10){
-        alert("Вы смотрите мало фильмов");
-    } else if ((10 < count) && (count < 30)){
-        alert("Вы классический зритель");
-    } else if (30 < count) {
-        alert("Вы киноман!");
-    } else {
-        alert("Ошибка");
-    }
-}
-
-function showMyDB(){
-    if (!personalMovieDB.privat){
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGeneres(){
-    for (let i = 0; i < 3; i++){
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, "");
-    }
-}
-
 
 const personalMovieDB = {
     count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function(){
+        let numberOfFilms;
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        while (numberOfFilms == '' || numberOfFilms == null || typeof(numberOfFilms) != typeof(123)){
+            numberOfFilms = 
+            +prompt("Ваш прошлый ответ неуместен!"+
+            "\nОтветьте еще раз, пожалуйста!\nСколько фильмов вы уже посмотрели?", "");
+        }
+        this.count = numberOfFilms;
+    },
+    rememberFilms: function(){
+        for (let i = 0; i < 3; i++){
+            let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+                mark = prompt("На сколько вы его оцените?", ""),
+                check = lastFilm.length < 50;
+            while (!(check)||!(lastFilm)||!(mark)){
+                lastFilm = 
+                prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз, пожалуйста!\n" + 
+                "Один из последних просмотренных фильмов?", "");
+                mark = prompt("Ваш прошлый ответ неуместен!\nОтветьте еще раз, пожалуйста!" + 
+                "\nНа сколько вы его оцените?", "");
+                check = (lastFilm.length < 50);
+            }
+            this.movies[lastFilm] = mark;
+        }
+    },
+    isKinoman: function(count){
+        if (count < 10){
+            alert("Вы смотрите мало фильмов");
+        } else if ((10 < count) && (count < 30)){
+            alert("Вы классический зритель");
+        } else if (30 < count) {
+            alert("Вы киноман!");
+        } else {
+            alert("Ошибка");
+        }
+    },
+    showMyDB: function(privat){
+        if (!privat){
+            console.log(this);
+        }
+    },
+    writeYourGeneres: function(){
+        for (let i = 0; i < 3; i++){
+            this.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, "");
+        }
+        this.genres.forEach(function(item, i){
+            console.log(`Любимый жанр #${i+1} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: () => {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    }
 };
-start();
-rememberFilms();
-isKinoman(personalMovieDB.count);
-writeYourGeneres();
-showMyDB();
+personalMovieDB.start();
+personalMovieDB.rememberFilms();
+personalMovieDB.isKinoman(personalMovieDB.count);
+personalMovieDB.writeYourGeneres();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB(personalMovieDB.privat);
 //console.log(personalMovieDB.count);
 
 //console.log(personalMovieDB);
